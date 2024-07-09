4d44172e29c22aa89f43baa45d229cdb83ba0654
Jhonatan <jhonatanwcom@gmail.com>
2024-07-09 15:25:18 -0300

    first

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -1,17 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
+  return(
     <View style={styles.container}>
-      <Text style={{ color: '#fff' }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+    <Text> Hi there!!</Text>
     </View>
-  );
+  )
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
