9901a1b3af45a8851a128d4dbcf02e54db1e3080
Jhonatan <jhonatanwcom@gmail.com>
2024-07-17 15:22:17 -0300

    update

--- StickerSmash/components/EmojiSticker.js
+++ StickerSmash/components/EmojiSticker.js
@@ -46,7 +46,7 @@ export default function EmojiSticker({ imageSize, stickerSource }) {
 
   return (
     <GestureDetector gesture={drag}>
-      <Animated.View style={[containerStyle, {top: -350} ]}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
         <GestureDetector gesture={doubleTap}>
           <Animated.Image
             source={stickerSource}
